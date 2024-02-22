bcacc923e8f99db7db3ddfd48be92b896422e0f5
CagriDK <cagridknl@gmail.com>
2024-02-22 19:29:15 +0300

    Test data structure fixed. The data will come through a tcp/udp socket and will be a std::vector<char> or char * type of data. The test has fixed the issue.

--- src/RadarClientMessageHandler.cpp
+++ src/RadarClientMessageHandler.cpp
@@ -10,14 +10,9 @@ RadarClientMessageHandler::~RadarClientMessageHandler()
 
 void RadarClientMessageHandler::asterixMessageParser(const std::vector<char> &data)
 {
-    // size_t byte_array_size = data.size() / 2;
-    // char data_bytes[byte_array_size];
-    // hexStringToBytes(data, data_bytes, byte_array_size);
-
-    const char *data1 = "\x30\x00\x30\xFD\xF7\x02\x19\xC9\x35\x6D\x4D\xA0\xC5\xAF\xF1\xE0\x02\x00\x05\x28\x3C\x66\x0C\x10\xC2\x36\xD4\x18\x20\x01\xC0\x78\x00\x31\xBC\x00\x00\x40\x0D\xEB\x07\xB9\x58\x2E\x41\x00\x20\xF5";
-    const size_t byte_array_size = 48;
-    char data_bytes[byte_array_size]; // Char dizisini tanımla
-    memcpy(data_bytes, data1, byte_array_size); // data'yı data_bytes'a kopyala
+    size_t byte_array_size = data.size() / 2;
+    char data_bytes[byte_array_size];
+    hexStringToBytes(data, data_bytes, byte_array_size);
 
     int catType = static_cast<int>(static_cast<unsigned char>(data_bytes[0]));
     json jData;
