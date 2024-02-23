04abf24351551ff7d78f0922b9791dfc79934cc8
CagriDK <cagridknl@gmail.com>
2024-02-23 12:34:07 +0300

    extendablebitsitemserializer added

--- CATMessages/tools/src/serializer/extendablebitsitemserializer.cpp
+++ CATMessages/tools/src/serializer/extendablebitsitemserializer.cpp
@@ -26,5 +26,63 @@ void ExtendableBitsItemSerializer::serializeItem(nlohmann::json &jData, size_t i
                                size_t current_parsed_bytes, std::vector<char> &target, 
                                bool debug) 
 {
+    json current_data = jData;
+    std::vector<unsigned char> temp_target;
 
+    if(data_type_ == "bitfield")
+    {
+        bool value = true;
+        std::vector<bool> available = current_data[name_];
+        unsigned int parsed_bytes = 1;
+
+        if(reverse_order_)
+        {
+            std::reverse(available.begin(), available.end());
+        }
+
+        while(value != false)
+        {
+            unsigned char convertedByte = 0;
+            std::vector<bool> tempVec;
+
+            for(size_t cnt{0}; cnt < 8; ++cnt)
+            {
+                tempVec.push_back(available[cnt] * parsed_bytes);
+            }
+
+            if(reverse_bits_)
+            {
+
+            }
+            else
+            {
+                std::transform(tempVec.begin(),tempVec.end(),tempVec.begin(), [](bool val) {return !val;});
+            }
+
+            for(size_t cnt{0}; cnt < tempVec.size(); ++cnt)
+            {
+                if(tempVec[cnt])
+                {
+                    convertedByte |= 1 << (7 - cnt);
+                }
+            }
+
+            if(reverse_order_)
+            {
+                value = tempVec[(parsed_bytes -1) * 8];
+            }
+            else{ 
+                value = tempVec[(8 * parsed_bytes) - 1];
+            }
+        
+            ++parsed_bytes;
+            target.push_back(convertedByte);
+        }
+
+        //{[0] 1 0 0 1 0 0 1} => LSB FSPEC biti value check ilk bit yada son byteın -7 değerinden baslayacak
+    }
+    else
+    {
+        throw runtime_error("extendable bits item '" + name_ + "' parsing without data type '" + data_type_ + "'");
+    }
 }
