a41aa91997cb84b87db0fbe3c92d0d90f3c1d5dd
CagriDK <cagridknl@gmail.com>
2024-02-23 12:33:54 +0300

    compounditemserializer added

--- CATMessages/tools/src/serializer/compounditemserializer.cpp
+++ CATMessages/tools/src/serializer/compounditemserializer.cpp
@@ -17,7 +17,7 @@ CompoundItemSerializer::CompoundItemSerializer(const nlohmann::json& item_defini
         throw runtime_error("parsing compound item '" + name_ +
                             "' field specification is not an object");
 
-    field_specification_.reset(ItemSerializerBase::createItemParser(field_specification));
+    field_specification_.reset(ItemSerializerBase::createItemSerializer(field_specification));
     assert(field_specification_);
 
     if (!item_definition.contains("items"))
@@ -35,7 +35,7 @@ CompoundItemSerializer::CompoundItemSerializer(const nlohmann::json& item_defini
     for (const json& data_item_it : items)
     {
         item_name = data_item_it.at("name");
-        item = ItemSerializerBase::createItemParser(data_item_it); // leave out own name
+        item = ItemSerializerBase::createItemSerializer(data_item_it); // leave out own name
         assert(item);
         items_.push_back(std::unique_ptr<ItemSerializerBase>{item});
     }
@@ -45,5 +45,10 @@ void CompoundItemSerializer::serializeItem(nlohmann::json &jData, size_t index, 
                                size_t current_parsed_bytes, std::vector<char> &target, 
                                bool debug) 
 {
+    field_specification_->serializeItem(jData, 0, 0, 0, target, debug);
 
+    for(auto& df_item : items_)
+    {
+        df_item->serializeItem(jData, 0, 0, 0, target, debug);
+    }
 }
